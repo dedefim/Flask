85c50843275925e30a949b3bf97221289c013bb6
efim <dedefim56@gmail.com>
2023-03-26 00:54:05 +0500

    lesson2

--- blog/app.py
+++ blog/app.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask import render_template
+from blog.views.users import users_app
+from blog.views.articles import articles_app
+
+
 app = Flask(__name__)
+app.register_blueprint(users_app, url_prefix="/users")
+app.register_blueprint(articles_app, url_prefix="/articles")
 
 
 @app.route("/")
@@ -10,3 +17,13 @@ def index():
 @app.route("/greet/<name>/")
 def greet_name(name: str):
     return f"Hello {name}!"
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
