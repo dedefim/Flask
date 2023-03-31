14bc238686b477c8c22484e8f58020ecff7a94f1
efim <dedefim56@gmail.com>
2023-03-31 15:44:53 +0500

    FLASCK3

--- blog/app.py
+++ blog/app.py
@@ -4,9 +4,12 @@ from blog.views.users import users_app
 from blog.views.articles import articles_app
 
 
+
 app = Flask(__name__)
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
+
+
 
 
 @app.route("/")
