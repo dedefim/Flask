168a5d82d374aedb744c44ecbf41e34bcb1667ab
efim <dedefim56@gmail.com>
2023-04-21 23:13:22 +0500

    FLASCK10

--- api/autor.py
+++ api/autor.py
@@ -2,6 +2,9 @@ from flask_combo_jsonapi import ResourceDetail, ResourceList
 from blog.schemas import AuthorSchema
 from blog.models.database import db
 from blog.models import Author
+from combojsonapi.event.resource import EventsResource
+from blog.models import Author, Article
+
 
 class AuthorList(ResourceList):
     schema = AuthorSchema
@@ -10,9 +13,10 @@ class AuthorList(ResourceList):
         "model": Author,
     }
 
+
+
+class AuthorDetailEvents(EventsResource):
+    def event_get_articles_count(self, **kwargs):
+        return {"count": Article.query.filter(Article.author_id == kwargs["id"]).count()}
 class AuthorDetail(ResourceDetail):
-    schema = AuthorSchema
-    data_layer = {
-        "session": db.session,
-        "model": Author,
-    }
+    events = AuthorDetailEvents
